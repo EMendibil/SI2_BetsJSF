3901ddc9ce12535a17f9b0f6a8db2c95264144f2
EMendibil <83004847+EMendibil@users.noreply.github.com>
2021-12-17 00:39:11 +0100

    User hasita

--- src/eredua/domeinua/User.java
+++ src/eredua/domeinua/User.java
@@ -2,4 +2,35 @@ package eredua.domeinua;
 
 public class User {
 
+	private String userName;	
+	private String pasahitza;
+	private int mota; //0 balio du erabiltzaile arrunta bada eta 1 administradorea bada
+	private double dirKop;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPasahitza() {
+		return pasahitza;
+	}
+	public void setPasahitza(String pasahitza) {
+		this.pasahitza = pasahitza;
+	}
+	public int getMota() {
+		return mota;
+	}
+	public void setMota(int mota) {
+		this.mota = mota;
+	}
+	public double getDirKop() {
+		return dirKop;
+	}
+	public void setDirKop(double dirKop) {
+		this.dirKop = dirKop;
+	}
+	
+	
 }
